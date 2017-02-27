2ea16102a56d8f9c18b5099e8e40f324c5ebd650
Dan Ward <daniel.ward@forrent.com>
2017-02-27 09:29:49 -0500

    Add styles, clean up template for next exercise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,36 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+    <h1>{{name}}</h1>
+    <div [hidden]="hideAddress">
+      <fieldset>
+        <label>Street: </label>{{street}}
+      </fieldset>
+      <fieldset>
+        <label>City: </label>{{city}}
+      </fieldset>
+      <fieldset>
+        <label [style.color]="color">Region</label>
+        <select (change)="changeRegion($event.target.value)">
+          <option>north</option>
+          <option>south</option>
+          <option>east</option>
+          <option>west</option>
+        </select>
+      </fieldset>
+    </div>
+  `,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent {
+  name = 'Tom Bombadil';
+  city = 'Anytown';
+  street = '123 Main St';
+  region = 'east';
+  hideAddress = false;
+
+  public changeAddress() {
+
+  }
+
+}
