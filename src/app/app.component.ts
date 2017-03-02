45e19aeb3aeb7d4686e60fdab87fae5a1dd98e10
warlyware <danward@gmail.com>
2017-03-01 19:52:15 -0500

    Isolated styles FTW

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,34 +2,10 @@ import { Component } from '@angular/core';
 import { Customer } from './customer.model';
 
 @Component({
+  moduleId: module.id,
   selector: 'my-app',
-  template: `
-    <h1>{{customer.name}}</h1>
-
-    <input [(ngModel)]="customer.name" />
-    <label>
-      Hide Address:
-      <input [(ngModel)]="hideAddress" type="checkbox" />
-    </label>
-
-    <div [hidden]="hideAddress">
-      <fieldset>
-        <label>Street: <input [(ngModel)]="customer.address.street" /></label>
-      </fieldset>
-      <fieldset>
-        <label>City: </label>{{city}}
-      </fieldset>
-      <fieldset>
-        <label [style.color]="color">Region</label>
-        <select (change)="changeRegion($event.target.value)">
-          <option>north</option>
-          <option>south</option>
-          <option>east</option>
-          <option>west</option>
-        </select>
-      </fieldset>
-    </div>
-  `,
+  templateUrl: 'app.tpl.html',
+  styleUrls: ['app.component.css']
 })
 export class AppComponent {
 
