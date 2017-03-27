fdb816564fbbe1bead95618b94e5051d407f9ad3
Dan Ward <daniel.ward@forrent.com>
2017-03-27 12:15:20 -0400

    Fix indentation from 2 to 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,27 +3,27 @@ import { Component } from '@angular/core';
 import { Customer } from './customer.model';
 
 @Component({
-  moduleId: module.id,
-  selector: 'my-app',
-  templateUrl: 'app.tpl.html',
-  styleUrls: ['app.component.css']
+    moduleId: module.id,
+    selector: 'my-app',
+    templateUrl: 'app.tpl.html',
+    styleUrls: ['app.component.css']
 })
 export class AppComponent {
-  public regions = ['east', 'west', 'north', 'south', 'narnia', 'istanbul'];
-  public customers: Customer[] = [
+    public regions = ['east', 'west', 'north', 'south', 'narnia', 'istanbul'];
+    public customers: Customer[] = [
 
-  ];
+    ];
 
-  public customer: Customer = {
-    id: 1,
-    name: 'Tom Bombadil',
-    address: {
-      city: 'Anytown',
-      street: '123 Main St',
-      state: 'VA',
-      region: 'east'
-    }
-  };
+    public customer: Customer = {
+        id: 1,
+        name: 'Tom Bombadil',
+        address: {
+            city: 'Anytown',
+            street: '123 Main St',
+            state: 'VA',
+            region: 'east'
+        }
+    };
 
-  public hideAddress = false;
+    public hideAddress = false;
 }
